0c1f3c7edb5e9819a6e2e6db0c260d565096e976
Christian Negre <cnegre@lanl.gov>
2017-08-13 15:11:15 -0600

    increased number of test. Now testing for optimization

--- tests/run_test_lmp.sh
+++ tests/run_test_lmp.sh
@@ -7,7 +7,7 @@ MY_PATH=`pwd`                                   # Capturing the local path of th
 
 RUN=$HOME"/lammps/src/lmp_serial"  #EXAALT version of Lammps program
 
-for name in 0scf fullscf ; do
+for name in 0scf 2scf fullscf ; do
 
   INLATTEFILE="latte."$name".in"
   INLAMMPSFILE="in."$name
@@ -26,6 +26,30 @@ for name in 0scf fullscf ; do
   echo ""
 
   grep Energy out | sed -e s/"PAR"/$STRR/g  >  input_tmp.in
+  python ./tests/test-energy.py --reference $REF --current energy.out --reltol 0.0000001
+
+done
+
+# Tests for geometry optimizations 
+
+for name in opt ; do
+
+  INLATTEFILE="latte."$name".in"
+  INLAMMPSFILE="in."$name
+  REF="energy."$name".out"
+  COORDS="data."$name".lmp"
+
+  cp  ./tests/tests_lmp/$INLATTEFILE latte.in
+  cp  ./tests/tests_lmp/$INLAMMPSFILE .
+  cp  ./tests/tests_lmp/$REF .
+  cp  ./tests/tests_lmp/$COORDS .
+
+  echo -e "\nTesting for "$name" \n"
+
+  time $RUN < $INLAMMPSFILE >  out
+  grep -A 16 TotEng out | sed -e s/"TotEng"/"0.0"/g >  energy.out
+  echo ""
+  
   python ./tests/test-energy.py --reference $REF --current energy.out --reltol 0.0000001
 
 done
