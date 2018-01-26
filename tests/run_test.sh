0f2dfa81e9a5dfa101ddbd4b1f9937d8d9ea7f85
Christian F. A. Negre <christianfannegre@gmail.com>
2018-01-26 12:55:46 -0700

    test for kon

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -12,7 +12,7 @@ echo -e "\nTesting LATTE with new (latte.in) input files \n"
 mv latte.in latte.in.tmp
 
 for name in 0scf 2scf fullscf fullscf.etemp sp2 fullscf.nvt \
-       	fullscf.npt fullscf.vdw fullscf.spin ; do
+       	fullscf.npt fullscf.vdw fullscf.spin fullscf.kon ; do
 
   INLATTEFILE="latte."$name".in"
   REF="energy."$name".out"
