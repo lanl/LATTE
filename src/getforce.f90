26f973698b8be65bd6ecbfdea269891850edbe11
Christian F. A. Negre <christianfannegre@gmail.com>
2018-04-25 15:59:41 -0600

    Pulay computed with progress when PROGRESS= ON (#40)

--- src/getforce.f90
+++ src/getforce.f90
@@ -1,23 +1,23 @@
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!                                    
-! Copyright 2010.  Los Alamos National Security, LLC. This material was    !                                    
-! produced under U.S. Government contract DE-AC52-06NA25396 for Los Alamos !                                    
-! National Laboratory (LANL), which is operated by Los Alamos National     !                                    
-! Security, LLC for the U.S. Department of Energy. The U.S. Government has !                                    
-! rights to use, reproduce, and distribute this software.  NEITHER THE     !                                    
-! GOVERNMENT NOR LOS ALAMOS NATIONAL SECURITY, LLC MAKES ANY WARRANTY,     !                                    
-! EXPRESS OR IMPLIED, OR ASSUMES ANY LIABILITY FOR THE USE OF THIS         !                                    
-! SOFTWARE.  If software is modified to produce derivative works, such     !                                    
-! modified software should be clearly marked, so as not to confuse it      !                                    
-! with the version available from LANL.                                    !                                    
-!                                                                          !                                    
-! Additionally, this program is free software; you can redistribute it     !                                    
-! and/or modify it under the terms of the GNU General Public License as    !                                    
-! published by the Free Software Foundation; version 2.0 of the License.   !                                    
-! Accordingly, this program is distributed in the hope that it will be     !                                    
-! useful, but WITHOUT ANY WARRANTY; without even the implied warranty of   !                                    
-! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General !                                    
-! Public License for more details.                                         !                                    
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!                                    
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Copyright 2010.  Los Alamos National Security, LLC. This material was    !
+! produced under U.S. Government contract DE-AC52-06NA25396 for Los Alamos !
+! National Laboratory (LANL), which is operated by Los Alamos National     !
+! Security, LLC for the U.S. Department of Energy. The U.S. Government has !
+! rights to use, reproduce, and distribute this software.  NEITHER THE     !
+! GOVERNMENT NOR LOS ALAMOS NATIONAL SECURITY, LLC MAKES ANY WARRANTY,     !
+! EXPRESS OR IMPLIED, OR ASSUMES ANY LIABILITY FOR THE USE OF THIS         !
+! SOFTWARE.  If software is modified to produce derivative works, such     !
+! modified software should be clearly marked, so as not to confuse it      !
+! with the version available from LANL.                                    !
+!                                                                          !
+! Additionally, this program is free software; you can redistribute it     !
+! and/or modify it under the terms of the GNU General Public License as    !
+! published by the Free Software Foundation; version 2.0 of the License.   !
+! Accordingly, this program is distributed in the hope that it will be     !
+! useful, but WITHOUT ANY WARRANTY; without even the implied warranty of   !
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General !
+! Public License for more details.                                         !
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 SUBROUTINE GETFORCE
 
@@ -46,13 +46,22 @@ SUBROUTINE GETFORCE
 
            ! s/sp orbitals only so we can use the analytic code
 
+#ifdef PROGRESSON
+           IF (LATTEINEXISTS) THEN  !orthogonalize from progress lib if latte.in exists
+             CALL PULAY_SPPROGRESS
+           ELSE
+             CALL PULAY_SP
+           ENDIF           
+#else
            CALL PULAY_SP
+#endif
+
            IF (ELECTRO .EQ. 1) CALL FCOULNONO_SP
            IF (SPINON .EQ. 1) CALL FSPINNONO_SP
 
         ELSE
 
-           ! Otherwise use the complex but general expansions Josh Coe implemented 
+           ! Otherwise use the complex but general expansions Josh Coe implemented
            CALL PULAY
            IF (ELECTRO .EQ. 1) CALL FCOULNONO
            IF (SPINON .EQ. 1) CALL FSPINNONO
