5e9882ab1cb627e129226142052fbb34107ad179
Christian F. A. Negre <christianfannegre@gmail.com>
2018-06-07 15:45:18 -0600

    Added get_k_list

--- src/coulomb_ewald.f90
+++ src/coulomb_ewald.f90
@@ -29,117 +29,95 @@ SUBROUTINE COULOMBEWALD
 
   IMPLICIT NONE
 
-  INTEGER :: I, J, L, M, N
-  INTEGER :: LMIN, MMIN, NMIN
+  INTEGER :: I, J, L, M, N, IK
+  INTEGER :: LMIN, MMIN, NMIN, NK
   REAL(LATTEPREC) :: PREFACTOR, KEPREF, K2, K(3), DOT
   REAL(LATTEPREC) :: COSSUM, SINSUM, COSSUM2, SINSUM2
   REAL(LATTEPREC) :: FLL, FLM, FLN
   REAL(LATTEPREC) :: FORCE
   REAL(LATTEPREC) :: PREVIR, CORRFACT
   REAL(LATTEPREC) :: L11, L12, L13, M21, M22, M23
+
   IF (EXISTERROR) RETURN
 
-  LMIN = 0
+  NK = (2*LMAX+1)*(2*MMAX+1)*(2*NMAX+1)
 
-  ! LMAX, MMAX, NMAX computed during initialization
+  !$OMP PARALLEL DO DEFAULT(NONE) &
+  !$OMP SHARED(NATS,DELTAQ,K1_LIST,K2_LIST,K3_LIST,KSQ_LIST) &
+  !$OMP SHARED(KCUTOFF2,EIGHTPI,FOURCALPHA2,COULVOL,KECONST,CR) &
+  !$OMP PRIVATE(I,IK,DOT,FORCE,SINLIST,COSLIST,COSSUM,SINSUM) &
+  !$OMP PRIVATE(K2,PREFACTOR,PREVIR,COSSUM2,SINSUM2,KEPREF) &
+  !$OMP REDUCTION(+:FCOUL,VIRCOUL,COULOMBV)
+  DO IK = 1, NK
 
-  DO L = LMIN, LMAX
+     K2 = KSQ_LIST(IK)
 
-     IF (L .EQ. 0) THEN
-        MMIN = 0
-     ELSE
-        MMIN = -MMAX
-     ENDIF
+     IF (K2 .LE. KCUTOFF2) THEN
 
-     L11 = REAL(L)*RECIPVECS(1,1)
-     L12 = REAL(L)*RECIPVECS(1,2)
-     L13 = REAL(L)*RECIPVECS(1,3)
+        PREFACTOR = EIGHTPI*EXP( -K2/(FOURCALPHA2) ) / &
+             (COULVOL*K2)
 
-     DO M = MMIN, MMAX
+        PREVIR = (TWO/K2) + (TWO/(FOURCALPHA2))
 
-        NMIN = -NMAX
+        COSSUM = ZERO
+        SINSUM = ZERO
 
-        IF (L .EQ. 0 .AND. M .EQ. 0) NMIN = 1
+        ! Doing the sin and cos sums
 
-        M21 = L11 + REAL(M)*RECIPVECS(2,1)
-        M22 = L12 + REAL(M)*RECIPVECS(2,2)
-        M23 = L13 + REAL(M)*RECIPVECS(2,3)
+        DO I = 1, NATS
 
-        DO N = NMIN, NMAX
+           DOT = K1_LIST(IK)*CR(1,I) + K2_LIST(IK)*CR(2,I) + K3_LIST(IK)*CR(3,I)
 
-           K(1) = M21 + REAL(N)*RECIPVECS(3,1)
-           K(2) = M22 + REAL(N)*RECIPVECS(3,2)
-           K(3) = M23 + REAL(N)*RECIPVECS(3,3)
+           ! We re-use these in the next loop...
 
-           K2 = K(1)*K(1) + K(2)*K(2) + K(3)*K(3)
+           SINLIST(I) = SIN(DOT)
+           COSLIST(I) = COS(DOT)
 
-           IF (K2 .LE. KCUTOFF2) THEN
-
-              PREFACTOR = EIGHTPI*EXP( -K2/(FOURCALPHA2) ) / &
-                   (COULVOL*K2)
-
-              PREVIR = (TWO/K2) + (TWO/(FOURCALPHA2))
-
-              COSSUM = ZERO
-              SINSUM = ZERO
-
-              ! Doing the sin and cos sums
-
-              DO I = 1, NATS
-
-                 DOT = K(1)*CR(1,I) + K(2)*CR(2,I) + K(3)*CR(3,I)
-
-                 ! We re-use these in the next loop...
-
-                 SINLIST(I) = SIN(DOT)
-                 COSLIST(I) = COS(DOT)
-
-                 COSSUM = COSSUM + DELTAQ(I)*COSLIST(I)
-                 SINSUM = SINSUM + DELTAQ(I)*SINLIST(I)
-
-              ENDDO
-
-              COSSUM2 = COSSUM*COSSUM
-              SINSUM2 = SINSUM*SINSUM
-
-              ! Add up energy and force contributions
-
-              KEPREF = KECONST*PREFACTOR
-
-              DO I = 1, NATS
-
-                 COULOMBV(I) = COULOMBV(I) +  &
-                      KEPREF*(COSLIST(I)*COSSUM + SINLIST(I)*SINSUM)
-
-                 FORCE = KEPREF * DELTAQ(I) * &
-                      (SINLIST(I)*COSSUM - COSLIST(I)*SINSUM)
-
-                 FCOUL(1,I) = FCOUL(1,I) + FORCE*K(1)
-                 FCOUL(2,I) = FCOUL(2,I) + FORCE*K(2)
-                 FCOUL(3,I) = FCOUL(3,I) + FORCE*K(3)
-
-              ENDDO
-
-              KEPREF = -KEPREF * (COSSUM2 + SINSUM2)/TWO
-
-              VIRCOUL(1) = VIRCOUL(1) + KEPREF * &
-                   (ONE - PREVIR*K(1)*K(1))
-              VIRCOUL(2) = VIRCOUL(2) + KEPREF * &
-                   (ONE - PREVIR*K(2)*K(2))
-              VIRCOUL(3) = VIRCOUL(3) + KEPREF * &
-                   (ONE - PREVIR*K(3)*K(3))
-              VIRCOUL(4) = VIRCOUL(4) - KEPREF*PREVIR* &
-                   K(1)*K(2)
-              VIRCOUL(5) = VIRCOUL(5) - KEPREF*PREVIR* &
-                   K(2)*K(3)
-              VIRCOUL(6) = VIRCOUL(6) - KEPREF*PREVIR* &
-                   K(3)*K(1)
-
-           ENDIF
+           COSSUM = COSSUM + DELTAQ(I)*COSLIST(I)
+           SINSUM = SINSUM + DELTAQ(I)*SINLIST(I)
 
         ENDDO
-     ENDDO
-  ENDDO
+
+        COSSUM2 = COSSUM*COSSUM
+        SINSUM2 = SINSUM*SINSUM
+
+        ! Add up energy and force contributions
+
+        KEPREF = KECONST*PREFACTOR
+
+        DO I = 1, NATS
+
+           COULOMBV(I) = COULOMBV(I) +  &
+                KEPREF*(COSLIST(I)*COSSUM + SINLIST(I)*SINSUM)
+
+           FORCE = KEPREF * DELTAQ(I) * &
+                (SINLIST(I)*COSSUM - COSLIST(I)*SINSUM)
+
+           FCOUL(1,I) = FCOUL(1,I) + FORCE*K1_LIST(IK)
+           FCOUL(2,I) = FCOUL(2,I) + FORCE*K2_LIST(IK)
+           FCOUL(3,I) = FCOUL(3,I) + FORCE*K3_LIST(IK)
+
+        ENDDO
+
+        KEPREF = -KEPREF * (COSSUM2 + SINSUM2)/TWO
+
+        VIRCOUL(1) = VIRCOUL(1) + KEPREF * &
+             (ONE - PREVIR*K1_LIST(IK)*K1_LIST(IK))
+        VIRCOUL(2) = VIRCOUL(2) + KEPREF * &
+             (ONE - PREVIR*K2_LIST(IK)*K2_LIST(IK))
+        VIRCOUL(3) = VIRCOUL(3) + KEPREF * &
+             (ONE - PREVIR*K3_LIST(IK)*K3_LIST(IK))
+        VIRCOUL(4) = VIRCOUL(4) - KEPREF*PREVIR* &
+             K1_LIST(IK)*K2_LIST(IK)
+        VIRCOUL(5) = VIRCOUL(5) - KEPREF*PREVIR* &
+             K2_LIST(IK)*K3_LIST(IK)
+        VIRCOUL(6) = VIRCOUL(6) - KEPREF*PREVIR* &
+             K3_LIST(IK)*K1_LIST(IK)
+
+     ENDIF
+
+  END DO
+  !$OMP END PARALLEL DO
 
   ! Point self energy
 
@@ -150,4 +128,3 @@ SUBROUTINE COULOMBEWALD
   RETURN
 
 END SUBROUTINE COULOMBEWALD
-
