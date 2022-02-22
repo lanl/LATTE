302651d115e21fc0f8acbd5e762d6fc72e74f653
ares201005 <ares201005@gmail.com>
2022-02-22 09:50:02 -0700

    add graphsolver flag

--- src/bodirectprogress.F90
+++ src/bodirectprogress.F90
@@ -89,23 +89,24 @@ SUBROUTINE BOEVECSPRG
 
   ELSE ! This bit is for zero electronic temperature
 
-     !CALL PRG_BUILD_DENSITY_T0(ORTHOH_BML,ORTHOBO_BML,NUMTHRESH,BNDFIL,EVALS)
-     
-     ! Construct the graph out ot H^2 and apply threshold
-     CALL BML_ZERO_MATRIX(BML_MATRIX_ELLPACK, BML_ELEMENT_REAL, &
-       LATTEPREC, HDIM, HDIM, G_BML)
-     !call bml_multiply_x2(ORTHOH_bml,g_bml,1.0d-2,trace)
-     !call bml_threshold(g_bml, 10.0d0)
-     call bml_multiply_x2(ORTHOH_bml,g_bml,numthresh,trace)
-     call bml_threshold(g_bml, numthresh)
-     nparts = 8
-     E0 = CHEMPOT
+     IF (GRAPHON == 1) THEN
+       CALL PRG_BUILD_DENSITY_T0(ORTHOH_BML,ORTHOBO_BML,NUMTHRESH,BNDFIL,EVALS)
+     ELSE
+       ! Construct the graph out ot H^2 and apply threshold
+       CALL BML_ZERO_MATRIX(BML_MATRIX_ELLPACK, BML_ELEMENT_REAL, &
+         LATTEPREC, HDIM, HDIM, G_BML)
+       !call bml_multiply_x2(ORTHOH_bml,g_bml,1.0d-2,trace)
+       !call bml_threshold(g_bml, 10.0d0)
+       call bml_multiply_x2(ORTHOH_bml,g_bml,numthresh,trace)
+       call bml_threshold(g_bml, numthresh)
 
-     call prg_build_densityGP_T0(ORTHOH_BML, g_bml, ORTHOBO_bml, numthresh, bndfil, E0, nparts, verbose)
-     !call bml_print_matrix("rhoGP",ORTHOBO_bml,0,10,0,10)
-     CALL BML_DEALLOCATE(G_BML)
-
-     CALL BML_DEALLOCATE(ORTHOH_BML)
+       E0 = CHEMPOT
+       
+       call prg_build_densityGP_T0(ORTHOH_BML, g_bml, ORTHOBO_bml, numthresh, bndfil, E0, graph_nparts, verbose)
+       !call bml_print_matrix("rhoGP",ORTHOBO_bml,0,10,0,10)
+       CALL BML_DEALLOCATE(G_BML)
+       CALL BML_DEALLOCATE(ORTHOH_BML)
+     ENDIF 
 
   ENDIF
 
