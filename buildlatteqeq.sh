86b536db1d757982812e2fc4586c11c78739298b
ares201005 <ares201005@gmail.com>
2022-10-18 11:14:01 -0600

    add build script

---
+++ buildlatteqeq.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+topdir=$PWD
+echo $topdir
+if [ !-d "build" ]; then
+  mkdir build
+fi
+
+cd build
+
+# debug
+#cmake ../cmake -DCMAKE_INSTALL_PREFIX=$topdir/install -DCMAKE_Fortran_FLAGS='-O0 -g -traceback -check all -debug all -check all -fpe0' #-DOPENMP=${CMAKE_WITH_PROGRESS} 
+
+# release
+cmake ../cmake -DCMAKE_INSTALL_PREFIX=$topdir/install -DCMAKE_Fortran_FLAGS='-O3 -fpp' #-DOPENMP=${CMAKE_WITH_PROGRESS} 
+
+VERBOSE=2 make
+make install
+
+#cd ~/path/to/lammps_compute_pace/build_latte/
+#rm CMakeFiles/lammps.dir/vast/home/zhy/ecp/lammps/src/LATTEQEQ/fix_latteqeq.cpp.o
+#~/path/to/lammps_compute_pace//build_pace_latteqeq.sh 
+#make -j
