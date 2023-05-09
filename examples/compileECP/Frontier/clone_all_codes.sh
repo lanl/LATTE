179bd18621ca5f218a5122d5c1a66c6135934c83
cnegre <christianfannegre@gmail.com>
2023-05-09 16:11:49 -0600

    Changes in Frontier compile

--- examples/compileECP/Frontier/clone_all_codes.sh
+++ examples/compileECP/Frontier/clone_all_codes.sh
@@ -2,5 +2,6 @@
 rm -rf bml; git clone git@github.com:lanl/bml.git
 rm -rf qmd-progress; git clone git@github.com:lanl/qmd-progress.git
 rm -rf LATTE; git clone -b ECP git@github.com:lanl/LATTE.git
-rm -rf lammps; git clone https://github.com/lammps/lammps.git
+rm -rf lammps; tar -xvzf patch_23Jun2022_update2.tar.gz; mv patch_23Jun2022_update2 lammps
+
 
