8da8bab70b6e59fdf66d080122763a1281ac619b
cnegre <christianfannegre@gmail.com>
2023-05-10 00:44:31 -0400

    Adding files

---
+++ examples/compileECP/Frontier/systems/h2o/job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -A CSC304
+#SBATCH -J uo2
+#SBATCH -o %x-%j.out
+#SBATCH -t 00:30:00
+#SBATCH -p batch
+#SBATCH -N 1
+export OMP_NUM_THREADS=8
+module load PrgEnv-gnu
+module load magma/2.6.1
+module load cmake
+module load openblas
+module load rocm/5.2.0 
+
+$MEMBERWORK/csc304/Frontier1/lammps/src/lmp_serial < in.md 
